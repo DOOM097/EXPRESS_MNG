9d2a6baa06584241d9ea3c0f71f4d62fed8bc86b
DOOM097 <98549250+DOOM097@users.noreply.github.com>
2024-06-18 00:42:49 +0300

    Add files via upload

---
+++ Node-express-master/index.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+const exphbs = require('express-handlebars');
+const Handlebars = require('handlebars');
+// npm install @handlebars/allow-prototype-access
+const { allowInsecurePrototypeAccess } = require('@handlebars/allow-prototype-access');
+
+const homeRoutes = require('./routes/home');
+const cardRoutes = require('./routes/card');
+const addRoutes = require('./routes/add');
+const coursesRoutes = require('./routes/courses');
+
+const app = express();
+
+const hbs = exphbs.create({
+  defaultLayout: 'main',
+  extname: 'hbs',
+  handlebars: allowInsecurePrototypeAccess(Handlebars)
+});
+
+app.engine('hbs', hbs.engine);
+
+app.set('view engine', 'hbs');
+app.set('views', 'views');
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ 
+    extended: true 
+}));
+
+app.use('/', homeRoutes);
+app.use('/add', addRoutes);
+app.use('/courses', coursesRoutes);
+app.use('/card', cardRoutes);
+
+const PORT = process.env.PORT || 3000;
+
+async function start() {
+  try {
+    const url = 'mongodb+srv://nemchenkoe2005:kp5NoBUPYpfkkkvN@cluster0.agqvgtr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+    await mongoose.connect(url, {
+      useNewUrlParser: true,
+      
+    });
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error('Error connecting to MongoDB', error);
+  }
+}
+
+start();
